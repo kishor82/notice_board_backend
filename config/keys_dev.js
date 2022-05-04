2dc599a7e8d4e87efab2f9bef8dcaac9f37f8763
kishor82 <kishorrathva8298@gmail.com>
2022-05-05 02:04:45 +0530

    Removed keys_dev from gitingnore

---
+++ config/keys_dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  MongoURI: 'mongodb://localhost:27017/dev',
+  secretOrKey: 'Og3EmMLkTT5bl8QgBpfhLLOqKH5RB7cB36',
+  publicVapidKey:
+    'BGkJGGrQnMWl3sqOi_ufFT9dRMrkvS35bl8QgBpfhLLOqelZuwg4U-M9Pqy5kcDcy9a_nUIROtiTGHskq5DOstA',
+  privateVapidKey: 'iusfOsmPN8MSM5UPAUoI0EOg3EmMLkTTKH5RB7cB36Y',
+};
